d0dff931ecf2b55a082c2e89edb0b10e659d6838
David Valdivia <d.valdiviamartinez@gmail.com>
2020-12-15 19:51:52 +0100

    Event Handler base

---
+++ Data Analysis Delivery 2/Assets/Data Analysis Scripts/EventData.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventData : MonoBehaviour
+{
+    uint eventID;
+
+    public string WriteJSON()
+    {
+        string json = JsonUtility.ToJson(this);
+        return json;
+    }
+
+    public EventData GetJSON(string json)
+    {
+        return JsonUtility.FromJson<EventData>(json); //TODO: This probably goes outside the class idk
+    }
+}
+
+public class DeathEvent : EventData
+{
+    Vector3 position;
+    Vector3 eulerAngles;
+}
+
+public class SessionEvent : EventData
+{
+    string userName;
+}
+
+public class DamageEvent : EventData
+{
+    Vector3 position;
+
+}
---
+++ Data Analysis Delivery 2/Assets/Data Analysis Scripts/Writer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Writer : MonoBehaviour
+{
+    public static Writer Instance;
+
+    private void Awake()
+    {
+        if (Instance == null)
+            Instance = this;
+        else
+            Destroy(gameObject);
+    }
+
+
+    public static void Print(string s)
+    {
+        Instance._Print(s);
+    }
+
+    private void _Print(string s)
+    {
+        //Write sql?
+        //Write csv
+    }
+
+
+}
