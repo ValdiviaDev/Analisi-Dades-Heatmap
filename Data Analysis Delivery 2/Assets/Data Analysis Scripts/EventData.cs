05d3dcaaf0ba38c606652b7f49640dd0d26feb0e
AitorVelez <aitorvelez@hotmail.com>
2020-12-16 17:57:33 +0100

    Events

--- Data Analysis Delivery 2/Assets/Data Analysis Scripts/EventData.cs
+++ Data Analysis Delivery 2/Assets/Data Analysis Scripts/EventData.cs
@@ -34,3 +34,34 @@ public class DamageEvent
     public Vector3 position;
 
 }
+
+public class TimeSpentEvent
+{
+    public float total_time_spent;
+}
+
+public class DestroyedCrateEvent
+{
+    public Vector3 position;
+    public float time_spent;
+    public int crates_destroyed;
+}
+
+public class DoorEvent
+{
+    public float time_spent;
+    public int door_num;
+}
+
+public class AttackEvent
+{
+    public int attacks_num;
+    public int enemies_killed;
+}
+
+public class HealingEvent
+{
+    public float time_spent;
+    public int hearts;
+    public int health_num;
+}
