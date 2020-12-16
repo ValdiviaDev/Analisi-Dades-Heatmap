bf3e86ae7b429ce13af85e9f53bf4990db5841a6
MAtaur00 <mtmtarres@gmail.com>
2020-12-16 17:26:28 +0100

    heatmap scripts

---
+++ Data Analysis Delivery 2/Assets/Data Analysis Scripts/HeatMap.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeatMap : MonoBehaviour
+{
+    int GridSize_X, GridSize_Y;
+    int[,] EventCounts;
+    public HeatMapCube HeatmapCubePrefab;
+
+    public Gradient ColorGradient;
+
+    public int MaxCounts = 100;
+
+    void CountEvents()
+    {
+        //for (int i = 0; i < CountEvents.Length; i++)
+        //{
+
+        //}
+    }
+    void VisualizeEvents()
+    {
+        EventCounts = new int[GridSize_X, GridSize_Y];
+        for (int i = 0; i < GridSize_X; i++)
+        {
+            for (int j = 0; j < GridSize_Y; j++)
+            {
+                SpawnCube(i, j, EventCounts[i, j]);
+            }
+
+        }
+    }
+
+    private void SpawnCube(int x, int y, int counts)
+    {
+        Vector3 pos = new Vector3(x * GridSize_X, y * GridSize_Y);
+        var cube = Instantiate(HeatmapCubePrefab, pos, Quaternion.identity) as HeatMapCube;
+        float f = Mathf.Clamp01((float)counts / MaxCounts);
+        Color c = ColorGradient.Evaluate(f);
+        cube.SetColor(c);
+    }
+}
---
+++ Data Analysis Delivery 2/Assets/Data Analysis Scripts/HeatMapCube.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeatMapCube : MonoBehaviour
+{
+    public void SetColor(Color c)
+    {
+        GetComponent<Renderer>().material.color = c;
+    }
+}
