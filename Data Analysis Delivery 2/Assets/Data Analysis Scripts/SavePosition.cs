c46a23148742be4763fbe66d0637d4132e5ce12d
VictorTirado <victor.tirado@innovamat.com>
2020-12-20 14:22:30 +0100

    Save position en csv

---
+++ Data Analysis Delivery 2/Assets/Data Analysis Scripts/SavePosition.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavePosition : MonoBehaviour
+{
+    public EventHandler eventhandler = null;
+    float timer = 0.0f;
+    public float time2save = 0.3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= time2save)
+        {
+            eventhandler.NewPositionsEvent();
+            timer = 0.0f;
+        }
+    }
+}
